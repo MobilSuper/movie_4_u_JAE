c8609214dc45bd503f70564e312b79645e38856d
MobilSuper <ahnhjeng@gmail.com>
2024-01-31 16:23:15 +0900

    update commit

--- job01_crawling.py
+++ job01_crawling.py
@@ -1,1 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import pandas as pd
+import re
+import time
+import datetime
+start_url = 'https://m.kinolights.com/discover/explore'
+options = ChromeOptions()
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+options.add_argument('user-agent=' + user_agent)
+options.add_argument('lang=ko_KR')
+# options.add_argument('headless')
+# options.add_argument('window-size=1366X768')
+
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+start_url='https://m.kinolights.com/discover/explore'
+button_movie_xpath='//*x'
+driver.get(start_url)
+time.sleep(0.5)
+driver.find_element(By.XPATH, button_movie_xpath)
+
+for url
