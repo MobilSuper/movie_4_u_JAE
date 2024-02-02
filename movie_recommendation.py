5b7d2824e2526397f709ee92ebb0d9e86ec311e8
MobilSuper <ahnhjeng@gmail.com>
2024-02-02 11:40:39 +0900

    update commit

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -17,10 +17,35 @@ df_reviews = pd.read_csv('./cleaned_one_review.csv')
 Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
-ref_idx = 10
-print(df_reviews.iloc[ref_idx, 0])
-consine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
-print(consine_sim[0])
-print(len(consine_sim))
-recommandation = getRecommendation(consine_sim)
-print(recommandation)
+
+
+
+# 영화 index 이용
+# ref_idx = 10
+# print(df_reviews.iloc[ref_idx, 0])
+# consine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
+# print(consine_sim[0])
+# print(len(consine_sim))
+# recommandation = getRecommendation(consine_sim)
+# print(recommandation)
+
+
+
+# 영화 keyward 이용
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '디즈니'
+sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+words = [keyword]
+for word, _ in sim_word:
+    words.append(word)
+sentence = []
+count = 10
+for word in words:
+    sentence = sentence + [word] * count
+    count -= 1
+sentence = ' '.join(sentence)
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+recommendation = getRecommendation(cosine_sim)
+
+print(recommendation)
